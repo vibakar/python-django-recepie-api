a7b8b02e7f98c2cc63170d41570a43109808479a
vibakar <vibakar23@gmail.com>
2024-06-20 19:40:31 +0100

    Fix for lint error

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,12 +4,12 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-
 from core import models
 
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
+
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
@@ -42,5 +42,6 @@ class UserAdmin(BaseUserAdmin):
             ),
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
